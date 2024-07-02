9aafedc01a51d5b3e902495c134cff9a3ef6dafc
Moaaz232 <168116418+Moaaz232@users.noreply.github.com>
2024-07-02 19:20:13 +0300

    Update main.js

--- main.js
+++ main.js
@@ -546,7 +546,7 @@ setInterval(async () => {
   if (stopped === 'close' || !conn || !conn.user) return;
   const _uptime = process.uptime() * 1000;
   const uptime = clockString(_uptime);
-  const bio = `[ ⏳ ] Uptime: ${uptime}`;
+  const bio = ` [ ⏳ ] وقت التشغيل: ${uptime}تم التطوير بواسطه𓆩🕸️S𝑷𝑰𝑫𝑬𝑹🕸️𓆪`;
   await conn.updateProfileStatus(bio).catch((_) => _);
 }, 60000);
 function clockString(ms) {
